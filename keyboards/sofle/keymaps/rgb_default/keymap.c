1085cbe8213ebc13f8c31520d2641758a50126ce
Ben Betz <bbetz@outlook.com>
2023-04-24 07:51:16 -0700

    Fix right hand layer

--- keyboards/sofle/keymaps/rgb_default/keymap.c
+++ keyboards/sofle/keymaps/rgb_default/keymap.c
@@ -20,50 +20,6 @@
 
 #include QMK_KEYBOARD_H
 
-#define INDICATOR_BRIGHTNESS 255 / 4
-
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-// Light combinations
-#define SET_INDICATORS(hsv) \
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, hsv}
-#define SET_UNDERGLOW(hsv) \
-	{1, 6, hsv}, \
-    {35+1, 6,hsv}
-#define SET_NUMPAD(hsv)     \
-	{35+15, 5, hsv},\
-	  {35+22, 3, hsv},\
-	  {35+27, 3, hsv}
-#define SET_NUMROW(hsv) \
-	{10, 2, hsv}, \
-		{20, 2, hsv}, \
-		{30, 2, hsv}, \
-	  {35+ 10, 2, hsv}, \
-	  {35+ 20, 2, hsv}, \
-	  {35+ 30, 2, hsv}
-#define SET_INNER_COL(hsv)	\
-	{33, 4, hsv}, \
-	  {35+ 33, 4, hsv}
-
-#define SET_OUTER_COL(hsv) \
-	{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}
-#define SET_THUMB_CLUSTER(hsv) 	\
-	{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
-#define SET_LAYER_ID(hsv) 	\
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-		{1, 6, hsv}, \
-    {35+1, 6, hsv}, \
-		{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}, \
-		{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
-
-
 enum sofle_layers {
     _DEFAULTS = 0,
     _QWERTY = 0,
@@ -211,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   _______,  KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, LGUI(KC_TAB),             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR, KC_RCBR,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, LGUI(LSFT(KC_TAB)),_______, _______, LCTL(LALT(KC_LEFT)), LCTL(LALT(KC_BSPC)), LCTL(LALT(KC_RGHT)),  KC_NO,   KC_BSLS,  KC_ENT,
+  _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, LGUI(LSFT(KC_TAB)),_______, C(A(KC_C)), C(A(KC_LEFT)), C(A(KC_DEL)), C(A(KC_RGHT)),  KC_NO,  KC_BSLS, KC_ENT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
                  _______, _______, _______, _______, _______,     KC_ENT,  KC_SPC, _______, _______, _______
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
@@ -330,14 +286,77 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef RGBLIGHT_ENABLE
-char layer_state_str[70];
+//char layer_state_str[70];
 // Now define the array of layers. Later layers take precedence
+
+#define INDICATOR_BRIGHTNESS 255 / 4
+
+#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
+#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
+
+// Light combinations
+#define SET_INDICATORS(hsv) \
+	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {36+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}
+#define SET_UNDERGLOW(hsv) \
+	{1, 6, hsv}, \
+    {36+1, 6,hsv}
+#define SET_NUMPAD(hsv)     \
+	{36+15, 5, hsv},\
+	  {36+22, 3, hsv},\
+	  {36+27, 3, hsv}
+#define SET_NUMROW(hsv) \
+	{12, 1, hsv}, \
+    {21, 2, hsv}, \
+	{31, 2, hsv}, \
+	  {36+ 12, 1, hsv}, \
+	  {36+ 21, 2, hsv}, \
+	  {36+ 31, 2, hsv}
+#define SET_ALPHAS(hsv) \
+    {13, 3, hsv}, \
+    {18, 3, hsv}, \
+    {23, 3, hsv}, \
+    {28, 3, hsv}, \
+    {33, 3, hsv},  \
+      {36+13, 3, hsv}, \
+      {36+18, 3, hsv}, \
+      {36+23, 3, hsv}, \
+      {36+28, 3, hsv}, \
+      {36+33, 3, hsv}
+// Inner col not working
+#define SET_INNER_COL(hsv)	\
+	{33, 4, hsv}, \
+	  {36+ 33, 4, hsv}
+
+#define SET_OUTER_COL(hsv) \
+	{8, 4, hsv}, \
+	  {36+ 8, 4, hsv}
+#define SET_THUMB_CLUSTER(hsv) 	\
+	{7, 1, hsv}, \
+    {16, 2, hsv}, \
+    {26, 2, hsv}, \
+	  {36+7, 1, hsv}, \
+      {36+16, 2, hsv}, \
+      {36+26, 2, hsv}
+#define SET_LAYER_ID(hsv) 	\
+	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {36+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+		{1, 6, hsv}, \
+    {36+1, 6, hsv}, \
+		{7, 4, hsv}, \
+	  {36+ 7, 4, hsv}, \
+		{25, 2, hsv}, \
+	  {36+ 25, 2, hsv}
 
 // QWERTY,
 // Light on inner column and underglow
 const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_RED)
-
+  SET_INDICATORS(HSV_RED),
+  SET_UNDERGLOW(HSV_BLUE),
+  SET_NUMROW(HSV_BLUE),
+  SET_OUTER_COL(HSV_BLUE),
+  SET_THUMB_CLUSTER(HSV_BLUE),
+  SET_ALPHAS(HSV_RED)
 );
 const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
   SET_LAYER_ID(HSV_PINK)
@@ -368,8 +387,8 @@ const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS
 	SET_NUMPAD(HSV_BLUE),
     {7, 4, HSV_ORANGE},
     {25, 2, HSV_ORANGE},
-    {35+6, 4, HSV_ORANGE},
-    {35+25, 2, HSV_ORANGE}
+    {36+6, 4, HSV_ORANGE},
+    {36+25, 2, HSV_ORANGE}
     );
 // _SWITCHER   // light up top row
 const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
