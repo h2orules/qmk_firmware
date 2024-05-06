64d7dedc4f7bc01356a5779eec038e1566be0859
Ben Betz <bbetz@outlook.com>
2024-05-06 08:16:36 -0700

    Update layout to fix shifts

--- keyboards/afternoonlabs/summer_breeze/keymaps/default/keymap.c
+++ keyboards/afternoonlabs/summer_breeze/keymaps/default/keymap.c
@@ -89,9 +89,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
            KC_BSLS, KC_LBRC, KC_RBRC,  LT(_NUMPAD,KC_TAB), KC_Q, KC_W, KC_E, KC_R,  KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC_DEL, KC_DEL, KC_END, KC_PGDN,
         //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
-                    KC_LPAD,           KC_CMTAB, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_UP,
+                    KC_LPAD,           CW_TOGG,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_UP,
         //├────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
-           DM_PLY1, DM_PLY2, KC_MCTL,  TD(TD_MEGA_SHIFT), KC_Z, KC_X, KC_C, KC_V,   KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,  KC_LEFT, KC_DOWN, KC_RIGHT,
+           DM_PLY1, DM_PLY2, KC_MCTL,  TD(TD_SHFT_CAPS), KC_Z, KC_X, KC_C, KC_V,   KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,  KC_LEFT, KC_DOWN, KC_RIGHT,
         //└────────┴────────┴────────┘└────────┴────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┴────────┴────────┴────────┘└────────┴────────┴────────┘
                                                                   KC_LCTL, KC_LOWER, KC_LGUI, KC_ENT,         KC_SPC,   KC_RGUI, KC_RAISE, KC_RALT
         //                                                       └────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┘
